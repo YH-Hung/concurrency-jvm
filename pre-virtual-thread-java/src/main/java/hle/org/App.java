53873cd0f458ebf15689d29b920546c636119216
yinghanhung <hle.infdy@gmail.com>
2026-01-31 16:29:39 +0800

    project init

---
+++ pre-virtual-thread-java/src/main/java/hle/org/App.java
@@ -0,0 +1,10 @@
+package hle.org;
+
+public final class App {
+    private App() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("pre-virtual-thread-java ready");
+    }
+}
---
+++ pre-virtual-thread-java/src/test/java/hle/org/AppTest.java
@@ -0,0 +1,12 @@
+package hle.org;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AppTest {
+    @Test
+    void sanityCheck() {
+        assertTrue(true);
+    }
+}
